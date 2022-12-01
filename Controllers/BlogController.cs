55f5d4be91bd9baa61db651eca358f1938c6735f
J4r0d-x55 <nino.business83@gmail.com>
2022-12-01 23:30:21 +0100

    first commit

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using BlogMVC.Models;
+using System.Linq;
+
+namespace BlogMVC.Controllers
+{
+    
+    public class BlogController : Controller
+    {
+        static List<BlogEntry> Posts = new List<BlogEntry>();
+
+        public IActionResult Index()
+        {
+            return View("Index", Posts);
+        }
+        public IActionResult CreatorPage(Guid id)
+        {
+            if (id != Guid.Empty)
+            {
+                BlogEntry existingEntry = Posts.FirstOrDefault(x => x.Id == id);
+                return View(model: existingEntry);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatorPage(BlogEntry entry)
+        {
+            if(entry.Id == Guid.Empty)
+            {
+                // New article
+
+                BlogEntry newEntry = new BlogEntry();
+                newEntry.Content = entry.Content;
+                newEntry.Id = Guid.NewGuid();
+                Posts.Add(newEntry);
+            }
+            else
+            {
+                // Existing article
+
+                BlogEntry existingEntry = Posts.FirstOrDefault(x => x.Id == entry.Id);
+                existingEntry.Content = entry.Content;
+            }
+           
+            return RedirectToAction("Index");
+        }
+    }
+}
